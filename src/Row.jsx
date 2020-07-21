f1e894ba53052b54f8561d6c5c712815739d5f19
damon5378 <iosifov.konstantin@gmail.com>
2020-07-21 21:32:15 +0300

    Added Nav component. Added trailer on youtube onClick on poster movie

--- src/Row.jsx
+++ src/Row.jsx
@@ -2,11 +2,15 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import instance from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseURL = "https://image.tmdb.org/t/p/original/";
 
 export const Row = (props) => {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
+
 
     useEffect(() => {
         async function fetchData() {
@@ -20,6 +24,28 @@ export const Row = (props) => {
         // if [], run once when the row loads and don't run again
     }, [props.fetchURL]);
 
+    const opts = {
+      height: "390",
+      width: "100%",
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+      },
+      autoplay: 1
+    }
+
+    const handleClick = (movie) => {
+      if(trailerUrl) {
+        setTrailerUrl('')
+      } else {
+        movieTrailer(movie?.name || "")
+        .then(url => {
+          // https://www.youtube.com/watch?v=${}
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'))
+        }).catch(error => console.log(error))
+      }
+    }
+
 
   return (
     <div className="row">
@@ -31,10 +57,12 @@ export const Row = (props) => {
             <img 
                 key={movie.id}
                 src={`${baseURL}${props.isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                onClick={ () => handleClick(movie) }
                 alt={movie.name}
                 className={`row__poster ${props.isLargeRow && "row__posterLarge"}`}/>
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
